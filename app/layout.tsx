40cc2df93767d699a7d156a91fc059f9e4b179d2
Augustin Laurent <augustin.lrt2@icloud.com>
2024-10-02 14:13:09 +0200

    Add speedinsights for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-Playfair`}
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
